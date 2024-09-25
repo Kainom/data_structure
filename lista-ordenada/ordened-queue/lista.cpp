07de1305564483cccfbda9acd9cb5d1a75e9a61c
Kainom <eleuteriokaina@gmail.com>
2024-09-25 15:00:49 -0300

    list ordered with vector in another mode,of minor value to major value

--- lista-ordenada/ordened-queue/lista.cpp
+++ lista-ordenada/ordened-queue/lista.cpp
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const int TAM = 3;
+const int TAM = 5;
 int elementos = 0;
 int lista[TAM];
 
 void insereLista(int value);
+void insereListaDecrescente(int value);
 void recuperaLista(int pos);
 void removeValue(int val);
 void removeLista(int pos);
@@ -13,108 +14,114 @@ void removeLista(int pos);
 int listaVazia();
 void imprime();
 
-int main()
-{
-    insereLista(1);
-    insereLista(3);
-    insereLista(2);
-    insereLista(4);
-    printf("List antes da remoção: ");
-    imprime();
-    // removeLista(2);
-    // removeLista(0);
-    // removeLista(0);
-    removeValue(3);
-    removeValue(3);
-    removeValue(2);
-    recuperaLista(0);
-    printf("List após remoção: ");
-    imprime();
+int main() {
+//   insereLista(1);
+//   insereLista(2);
+//   insereLista(4);
+//   insereLista(5);
+//   insereLista(3);
+insereListaDecrescente(1);
+insereListaDecrescente(2);
+insereListaDecrescente(4);
+insereListaDecrescente(5);
+insereListaDecrescente(0);
+imprime();
+  // // removeLista(2);
+  // // removeLista(0);
+  // // removeLista(0);
+  // removeValue(3);
+  // removeValue(3);
+  // removeValue(2);
+  // recuperaLista(0);
+  // printf("List após remoção: ");
+  // imprime();
 
-    return 0; // Indicate successful program execution
+  return 0; // Indicate successful program execution
 }
 
-// the firts rule for the  ordenated list is always put the value in the first position
-void insereLista(int value)
-{
-    int i = 0;
-    if (elementos == TAM)
-    {
-        printf("A lista está cheia!\n");
-        return;
-    }
+// the firts rule for the  ordenated list is always put the value in the first
+// position
+void insereLista(int value) { 
+  int i = 0;
+  if (elementos == TAM) { // check if list is full 
+    printf("A lista está cheia!\n");
+    return;
+  }
 
-    for (i = elementos; elementos > 0 && value < lista[i - 1]; i--) //
-    {
-        lista[i] = lista[i - 1];
-    }
+  for (i = elementos; elementos > 0 && value < lista[i - 1]; i--) //
+  {
+    lista[i] = lista[i - 1]; // move the value to next position 
+  }
 
-    lista[i] = value;
-    elementos++;
+  lista[i] = value;
+  elementos++;
 }
 
-void imprime()
-{
-    if (elementos == 0)
-    {
-        printf("A lista está vazia!\n");
-        return;
-    }
-
-    for (int i = 0; i < elementos; i++)
-    {
-        printf("%d ", lista[i]);
-    }
+void insereListaDecrescente(int value) {
+  int i = 0;
+  if (elementos == TAM) {
+    printf("A lista esta cheia!\n");
+    return;
+  }
+  for (i = elementos; i > 0 && value > lista[i - 1]; i--) {
+    lista[i] = lista[i - 1];
+  }
+  lista[i] = value;
+  elementos++;
 }
 
-int listaVazia()
-{
-    if (elementos == 0)
-    {
-        printf("A lista está vazia!\n");
-        return 1;
-    }
-    return 0;
+void imprime() {
+  if (elementos == 0) {
+    printf("A lista está vazia!\n");
+    return;
+  }
+
+  for (int i = 0; i < elementos; i++) {
+    printf("%d ", lista[i]);
+  }
 }
 
-void removeLista(int pos)
-{
-    if (listaVazia())
-    {
-        return;
-    }
-    if (pos < 0 || pos >= elementos)
-    {
-        printf("\nPosição inválida!\n");
-        return;
-    }
-
-    for (int i = pos; i < elementos - 1; i++) // 0 1 2  1 3 2
-    {
-        lista[i] = lista[i + 1];
-    }
-    elementos--;
+int listaVazia() {
+  if (elementos == 0) {
+    printf("A lista está vazia!\n");
+    return 1;
+  }
+  return 0;
 }
 
-void removeValue(int value)
-{
-    int i = 0;
-    if (listaVazia())
-    {
-        return;
-    }
+void removeLista(int pos) {
+  if (listaVazia()) {
+    return;
+  }
+  if (pos < 0 || pos >= elementos) {
+    printf("\nPosição inválida!\n");
+    return;
+  }
 
-    for (i = elementos - 1; i >= 0 && value != lista[i]; i--) // 0 1   1 2 
-    if (i>=0) removeLista(i); 
+  for (int i = pos; i < elementos - 1; i++) // 0 1 2  1 3 2
+  {
+    lista[i] = lista[i + 1];
+  }
+  elementos--;
 }
 
+void removeValue(int value) {
+  int i = 0;
+  if (listaVazia()) {
+    return;
+  }
 
-void recuperaLista(int pos){
-    if(listaVazia()) return;
-    if (pos < 0 || pos >= elementos)
-    {
-        printf("\nPosição inválida!\n");
-        return;
-    }
-    printf("\nO elemento na posição %d é: %d\n", pos, lista[pos]);
+  for (i = elementos - 1; i >= 0 && value != lista[i]; i--) // 0 1   1 2
+    if (i >= 0)
+      removeLista(i);
+}
+
+void recuperaLista(int pos) {
+  if (listaVazia())
+    return;
+  if (pos < 0 || pos >= elementos) {
+    printf("\nPosição inválida!\n");
+    return;
+  }
+  printf("\nO elemento na posição %d é: %d\n", pos, lista[pos]);
 }
