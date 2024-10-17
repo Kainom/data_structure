52a303c1ce84521e4a1cc16642fcc2d2df98c1ec
Kainom <eleuteriokaina@gmail.com>
2024-10-17 15:04:40 -0300

    Inserction sort

---
+++ sorting_algorithms/selection_sort/index.cpp
@@ -0,0 +1,51 @@
+#include "../utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int vetor[TAM];
+
+void selection();
+
+int main() {
+  clock_t t1, t2;
+  geraRandom();
+
+  t1 = clock();
+
+  desordenado();
+
+  t2 = clock();
+
+  double tempo = ((double)(t2 - t1)) / CLOCKS_PER_SEC;
+  printf("Tempo gasto no desordenado: %.6f segundos\n", tempo);
+
+  t1 = clock();
+  selection();
+  t2 = clock();
+
+  tempo = ((double)(t2 - t1)) / CLOCKS_PER_SEC;
+  printArray();
+  printf("Tempo gasto no ordenado: %.6f segundos\n", tempo);
+}
+
+void selection() {
+  int pivo = 0;
+  int j, menor;
+
+  for (int  i= 0; i < TAM; i++) {
+    pivo = i;
+
+    j = i + 1;
+    while (j <= (TAM - 1)) {
+      if (vetor[pivo] > vetor[j]) {
+        pivo = j;
+      }
+      j++;
+    }
+    menor = vetor[pivo];
+    vetor[pivo] = vetor[i];
+    vetor[i] = menor;
+  }
+}
